f381efaa76140ca338a3b5703c69472c6d3c877d
Frongo <frongosk8r@gmail.com>
2018-11-03 15:51:00 -0400

    Can die when you run out of water or sun and can spawn seeds

--- game/assets/scripts/plants.js
+++ game/assets/scripts/plants.js
@@ -1,7 +1,8 @@
 var plants = [];
 
-var PLANT_WATER_AMOUNT = 100;
-var PLANT_SUN_AMOUNT = 100;
+var PLANT_WATER_MAX = 100;
+var PLANT_SUN_MAX = 100;
+var PLANT_SEED_MAX = 100;
 
 var PLANT_MAX_LIFE_DAYS = 5.0;
 
@@ -13,7 +14,7 @@ var PLANT_WATER_ABSORB_PD = 100;
 var PLANT_SUN_USAGE_PD = 50;
 var PLANT_SUN_ABSORB_PD = 100;
 
-// Progress needed to advance to next level is PLANT_BASE_LEVEL_PROG + currentLevel * PLANT_LEVEL_PROG_MULTIPLIER
+var PLANT_SEED_PROGRESS_PD = 50;
 
 var PlantType = {
     NORMAL: "normal",
@@ -35,6 +36,7 @@ function plant_create(_position, _type)
         level: 0,
         water: 100,
         sun: 100,
+        seed: 0,
         dead: false,
         id: PlantData.globalId
     };
@@ -45,7 +47,7 @@ function plant_create(_position, _type)
     PlantData.globalId++;
 }
 
-function plant_progress(_plant, _amount)
+function plant_age(_plant, _amount)
 {
     // Dead
     if(_plant.dead || _plant.level < 3)
@@ -63,28 +65,35 @@ function plants_update(dt)
 {
     for(var i = 0; i < plants.length; ++i)
     {
-        plant_progress(plants[i], (dt / DayConstants.secondsPerDay) * DayConstants.timeScaleFactor);
+        plant_age(plants[i], (dt / DayConstants.secondsPerDay) * DayConstants.timeScaleFactor);
 
         if(!plants[i].dead)
         {
+            // Seed
+            if(plants[i].type == PlantType.SEED && plants[i].seed < PLANT_SEED_MAX)
+            {
+                plants[i].seed += PLANT_SEED_PROGRESS_PD * (dt / DayConstants.secondsPerDay) * DayConstants.timeScaleFactor;
+                plants[i].seed = Math.min(plants[i].seed, PLANT_SEED_MAX);
+            }
+
             // Water
             if(plants[i].type == PlantType.WATER && plants[i].level > 0)
             {
                 plants[i].water += PLANT_WATER_ABSORB_PD * weather_getWaterMultiplier() * plants[i].level * (dt / DayConstants.secondsPerDay) * DayConstants.timeScaleFactor;
                 plants[i].water -= PLANT_WATER_USAGE_PD * (dt / DayConstants.secondsPerDay) * DayConstants.timeScaleFactor;
-                plants[i].water = Math.min(plants[i].water, plants[i].level * PLANT_WATER_AMOUNT);
+                plants[i].water = Math.min(plants[i].water, plants[i].level * PLANT_WATER_MAX);
             }
             else
             {
                 plants[i].water += PLANT_WATER_ABSORB_PD * weather_getWaterMultiplier() * (dt / DayConstants.secondsPerDay) * DayConstants.timeScaleFactor;
                 plants[i].water -= PLANT_WATER_USAGE_PD * (dt / DayConstants.secondsPerDay) * DayConstants.timeScaleFactor;
-                plants[i].water = Math.min(plants[i].water, PLANT_WATER_AMOUNT);
+                plants[i].water = Math.min(plants[i].water, PLANT_WATER_MAX);
             }
 
             // Sunlight
             plants[i].sun += PLANT_SUN_ABSORB_PD * day_getLightLevel() * weather_getSunMultiplier() * (dt / DayConstants.secondsPerDay) * DayConstants.timeScaleFactor;
             plants[i].sun -= PLANT_SUN_USAGE_PD * (dt / DayConstants.secondsPerDay) * DayConstants.timeScaleFactor;
-            plants[i].sun = Math.min(plants[i].sun, PLANT_SUN_AMOUNT);
+            plants[i].sun = Math.min(plants[i].sun, PLANT_SUN_MAX);
 
             if(plants[i].sun <= 0 || plants[i].water <= 0)
             {
@@ -116,6 +125,11 @@ function plants_render()
         {
             SpriteBatch.drawRect(null, new Rect(plants[i].position - 8, -plants[i].water / 10, 2, plants[i].water / 10), new Color(0, 0, 1, 1));
             SpriteBatch.drawRect(null, new Rect(plants[i].position - 11, -plants[i].sun / 10, 2, plants[i].sun / 10), new Color(0, 1, 0, 1));
+        }
+
+        if(plants[i].seed == PLANT_SEED_MAX)
+        {
+            SpriteBatch.drawRect(null, new Rect(plants[i].position, -2, 2, 2), new Color(1, 1, 1, 1));
         }
     }
 }
