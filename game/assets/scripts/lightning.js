0f1c500c534b03839951e8145101ddedf6d09482
davidstl <daivuk@gmail.com>
2018-11-03 16:48:51 -0400

    stormy

---
+++ game/assets/scripts/lightning.js
@@ -0,0 +1,38 @@
+var lightning = {
+    timeTilNext: Random.randNumber(2, 4),
+    anim: new NumberAnim(0)
+};
+
+function lightning_init()
+{
+
+}
+
+function lightning_update(dt)
+{
+    lightning.timeTilNext -= dt;
+    if (lightning.timeTilNext <= 0)
+    {
+        lightning.timeTilNext = Random.randNumber(2, 4);
+        lightning.anim.stop();
+        lightning.anim.set(0);
+        lightning.anim.queue(2, .1, Tween.LINEAR);
+        lightning.anim.queue(0, .1, Tween.LINEAR);
+        lightning.anim.queue(4, .1, Tween.LINEAR);
+        lightning.anim.queue(0, .1, Tween.LINEAR);
+        lightning.anim.queue(2, .05, Tween.LINEAR);
+        lightning.anim.queue(0, .15, Tween.LINEAR);
+        lightning.anim.play();
+    }
+
+    var val = lightning.anim.get();
+    if (val > 0)
+    {
+        RGB.r = new Vector3(val);
+    }
+}
+
+function lightning_render()
+{
+
+}
