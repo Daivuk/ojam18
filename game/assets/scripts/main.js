490633e892c72555f3853262e2c3f9e21649ccc2
davidstl <daivuk@gmail.com>
2018-11-04 11:01:40 -0500

    removed temp shits

--- game/assets/scripts/main.js
+++ game/assets/scripts/main.js
@@ -48,17 +48,8 @@ weather_init();
 
 var ppp = null;
 plant_create(0, PlantType.SEED);
-plant_create(distanceBetweenPlants, PlantType.WATER);
-
-ppp = plant_create(distanceBetweenPlants * 2, PlantType.WATER);
-ppp.level = 1;
-ppp = plant_create(distanceBetweenPlants * 3, PlantType.WATER);
-ppp.level = 2;
-ppp = plant_create(distanceBetweenPlants * 4, PlantType.WATER);
-ppp.level = 3;
-
 fertile_ground_create(-distanceBetweenPlants);
-fertile_ground_create(distanceBetweenPlants * 5);
+fertile_ground_create(distanceBetweenPlants);
 
 var saveLoadTypes = ["Day", "Focus", "FertileGround", "Month", "Plant", "Season", "Weather", "Resource"];
 
