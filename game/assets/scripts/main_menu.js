58a6eaa0204aa53876cb737cbcfd2a559c26c261
davidstl <daivuk@gmail.com>
2018-11-04 12:28:13 -0500

    title scree is badass

--- game/assets/scripts/main_menu.js
+++ game/assets/scripts/main_menu.js
@@ -2,10 +2,15 @@ var MainMenuData = {
     isDisplaying: true
 };
 
+var titleTexture = getTexture("title.png");
+
 function main_menu_render()
 {
-    SpriteBatch.begin();
-    SpriteBatch.drawText(font, "Press Enter", new Vector2((resolution.x / 2) - (font.measure("Press Enter").x / 2), resolution.y / 2), new Color());
+    var scale = resolution.y / 240;
+    var scaledRes = new Vector2(resolution.x / scale, resolution.y / scale);
+    SpriteBatch.begin(Matrix.createScale(scale));
+    SpriteBatch.drawSprite(titleTexture, scaledRes.mul(new Vector2(0.5, .5)));
+    SpriteBatch.drawText(font, "Press Enter", new Vector2((scaledRes.x / 2) - (font.measure("Press Enter").x / 2), scaledRes.y * .7), new Color());
     SpriteBatch.end();
 }
 
