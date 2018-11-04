9dee516422ddfa7cde34d3b5f9eb96fdce9fccec
davidstl <daivuk@gmail.com>
2018-11-04 11:24:14 -0500

    resources icons n shit

--- game/assets/scripts/resources.js
+++ game/assets/scripts/resources.js
@@ -5,7 +5,9 @@ var ResourceConstants = {
 
 var ResourceData = {
     seeds: 1,
-    biomass: 1
+    biomass: 1,
+    seedsBounceAnim: new NumberAnim(0),
+    biomassBounceAnim: new NumberAnim(0)
 }
 
 var ResourceDataSaveProperties = [
@@ -15,11 +17,23 @@ var ResourceDataSaveProperties = [
 
 function resources_render()
 {
-    var resourcesBasePosition = new Vector2(resolution.x - 20, 10);
-    var seedsSpritePosition = new Vector2(resourcesBasePosition.x - 10, resourcesBasePosition.y);
-    var biomassSpritePosition = new Vector2(resourcesBasePosition.x + 10, resourcesBasePosition.y);
-    SpriteBatch.drawSpriteAnim(ResourceConstants.seedSprite, seedsSpritePosition);
-    SpriteBatch.drawSpriteAnim(ResourceConstants.biomassSprite, biomassSpritePosition);
-    SpriteBatch.drawText(font, "" + ResourceData.seeds, new Vector2(seedsSpritePosition.x, seedsSpritePosition.y + 5), new Vector2(), new Color());
-    SpriteBatch.drawText(font, "" + ResourceData.biomass, new Vector2(biomassSpritePosition.x, biomassSpritePosition.y + 5), new Vector2(), new Color());
+    var resourcesBasePosition = new Vector2(10, resolution.y / 2 - 30);
+    
+    if (ResourceData.seeds > 0)
+    {
+        var seedsSpritePosition = new Vector2(resourcesBasePosition.x + ResourceData.seedsBounceAnim.get() * 8, resourcesBasePosition.y);
+        SpriteBatch.drawInclinedRect(null, new Rect(0, seedsSpritePosition.y - 8, 38, 16), -0.3, new Color(0, 0, 0, .5));
+        SpriteBatch.drawSpriteAnim(ResourceConstants.seedSprite, seedsSpritePosition);
+        SpriteBatch.drawPrettyOutlinedText(font, "" + ResourceData.seeds, new Vector2(seedsSpritePosition.x + 10, seedsSpritePosition.y), Vector2.LEFT, Color.WHITE,
+            new Color(0, 0, 0, .5), 1);
+    }
+
+    if (ResourceData.biomass > 0)
+    {
+        var biomassSpritePosition = new Vector2(resourcesBasePosition.x + ResourceData.biomassBounceAnim.get() * 8, resourcesBasePosition.y + 20);
+        SpriteBatch.drawInclinedRect(null, new Rect(0, biomassSpritePosition.y - 8, 38, 16), -0.3, new Color(0, 0, 0, .5));
+        SpriteBatch.drawSpriteAnim(ResourceConstants.biomassSprite, biomassSpritePosition);
+        SpriteBatch.drawPrettyOutlinedText(font, "" + ResourceData.biomass, new Vector2(biomassSpritePosition.x + 10, biomassSpritePosition.y), Vector2.LEFT, Color.WHITE,
+            new Color(0, 0, 0, .5), 1);
+    }
 }
